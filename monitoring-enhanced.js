f80f23dc23f3567e0dd622c7131264d9335f471b
cannellsheila-star <cannellsheila@gmail.com>
2026-09-15 22:18:13 +0200

    Load grouped FusionSolar site monitoring

--- monitoring-enhanced.js
+++ monitoring-enhanced.js
@@ -1,1 +1,1 @@
-document.write('<script src="monitoring-enhanced-core.js"><\/script><script src="fusionsolar-status.js"><\/script>');
+document.write('<script src="monitoring-enhanced-core.js"><\/script><script src="fusionsolar-status.js"><\/script><script src="fusionsolar-sites.js"><\/script>');
